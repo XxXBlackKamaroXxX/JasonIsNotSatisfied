b6cf58547472e41fa681202aed4e4f6a97c11c6d
XxXBlackKamaroXxX <demakot@mail.ru>
2021-05-04 22:28:14 +1000

    githubmolodec

--- js_files/main.js
+++ js_files/main.js
@@ -212,8 +212,8 @@ function game() {
             evt.preventDefault(); 
             let touches = evt.changedTouches;
             moveTouch = touches[0];
-            plr.x = -last_x + moveTouch.pageX - 75;
-            plr.y = -last_y + moveTouch.pageY - 56;
+            plr.x = -last_x + moveTouch.pageX + 75;
+            plr.y = -last_y + moveTouch.pageY + 56;
             last_x = moveTouch.pageX;
             last_y = moveTouch.pageY;  
         }
